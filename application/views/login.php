9cd7dbfe7fc51889f4a6c8b5529431f1f612a749
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2025-05-06 16:16:31 +0700

    menambahkan vector agam di login

--- application/views/login.php
+++ application/views/login.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login DPMPTSP Kab. Agam</title>
 
+    <link href="<?= base_url('assets/'); ?>img/agam.png" rel="shortcut icon" type="image/png">
+
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" crossorigin="anonymous">
     <!-- Font Awesome -->
