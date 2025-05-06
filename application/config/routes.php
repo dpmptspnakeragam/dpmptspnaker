8adc5af71469731aef945a72cf5670975f87a134
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2025-05-06 15:59:15 +0700

    menambahkan error page 404

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'home';
-$route['404_override'] = '';
+$route['404_override'] = 'error_page/page_missing';
 $route['translate_uri_dashes'] = FALSE;
 
 
