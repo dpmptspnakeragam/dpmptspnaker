20044b5e3e14be0807d6757f65380369cf1adf8d
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2025-05-16 15:29:55 +0700

    update view_perizinan_resiko

--- application/views/view_perizinan_risiko.php
+++ application/views/view_perizinan_risiko.php
@@ -24,65 +24,72 @@
             <div class="container-fluid">
                 <div class="card mb-3">
                     <div class="card-header">
-                        <i class="fa fa-table"></i> Prosedur dan Persyaratan
+                        <i class="fa fa-table"></i> Prosedur
                     </div>
                     <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-12 text-center">
-                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                    <ol class="carousel-indicators carousel-risiko">
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                                    </ol>
-                                    <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <img class="gambar-risiko" src="<?= base_url() ?>assets/img/risiko_rendah.jpg?>" alt="Risiko Rendah">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img class="gambar-risiko" src="<?= base_url() ?>assets/img/risiko_menengah_rendah.jpg?>" alt="Risiko Menengah Rendah">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img class="gambar-risiko" src="<?= base_url() ?>assets/img/risiko_menengah_tinggi.jpg?>" alt="Risiko Menengah Tinggi">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img class="gambar-risiko" src="<?= base_url() ?>assets/img/risiko_tinggi.jpg?>" alt="Risiko Tinggi">
-                                        </div>
+                        <div class="text-center">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators carousel-risiko">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img class="gambar-risiko img-thumbnail" src="<?= base_url() ?>assets/img/risiko_rendah.jpg?>" alt="Risiko Rendah">
                                     </div>
-                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                        <span class="carousel-control-prev-icon kontrol-risiko" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                        <span class="carousel-control-next-icon kontrol-risiko" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
+                                    <div class="carousel-item">
+                                        <img class="gambar-risiko img-thumbnail" src="<?= base_url() ?>assets/img/risiko_menengah_rendah.jpg?>" alt="Risiko Menengah Rendah">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="gambar-risiko img-thumbnail" src="<?= base_url() ?>assets/img/risiko_menengah_tinggi.jpg?>" alt="Risiko Menengah Tinggi">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="gambar-risiko img-thumbnail" src="<?= base_url() ?>assets/img/risiko_tinggi.jpg?>" alt="Risiko Tinggi">
+                                    </div>
                                 </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon kontrol-risiko" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon kontrol-risiko" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
                             </div>
                         </div>
-                        <hr>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fa fa-table"></i> Persyaratan
+                    </div>
+                    <div class="card-body">
                         <div class="row">
                             <div class="table-responsive">
                                 <table class="table table-striped table-borderless table-hover" id="dataTable" width="100%" cellspacing="0">
                                     <thead class="bg-dark text-light">
                                         <tr>
-                                            <th>Jenis Risiko</th>
-                                            <th>Biaya</th>
-                                            <th>Lama Proses</th>
-                                            <th>Persyaratan</th>
+                                            <th class="text-center align-middle">No.</th>
+                                            <th class="text-center align-middle">Jenis Risiko</th>
+                                            <th class="text-center align-middle">Biaya</th>
+                                            <th class="text-center align-middle">Lama Proses</th>
+                                            <th class="text-center align-middle">Persyaratan</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <?php
-                                        $no = 1;
+                                        $count = 1;
                                         foreach ($perizinan->result() as $row) {
                                         ?>
                                             <tr>
-                                                <td><?= $row->jenis; ?></td>
-                                                <td><?= $row->biaya; ?></td>
-                                                <td><?= $row->lamaproses; ?></td>
-                                                <td><?= $row->syarat; ?></td>
+                                                <td class="text-center align-middle"><?= $count++; ?></td>
+                                                <td class="text-center align-middle"><?= $row->jenis; ?></td>
+                                                <td class="text-center align-middle"><?= $row->biaya; ?></td>
+                                                <td class="text-center align-middle"><?= $row->lamaproses; ?></td>
+                                                <td class="text-center align-middle"><?= $row->syarat; ?></td>
                                             </tr>
                                         <?php } ?>
                                     </tbody>
