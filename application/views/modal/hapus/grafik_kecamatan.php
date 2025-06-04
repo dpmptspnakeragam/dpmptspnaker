37b0bfb08929d529279f9876aa45b9f5bb960355
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2025-06-04 14:52:59 +0700

    update tampilan modal hapus nib, kecamatan, risiko, dan kbli pada halaman admin

--- application/views/modal/hapus/grafik_kecamatan.php
+++ application/views/modal/hapus/grafik_kecamatan.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Hapus <?= $title; ?></h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Hapus Sebaran Proyek Per Kecamatan Usaha</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
